abf77ace428d8fc162df498797221f9520d51079
sionkim <tkdlqhrm222@gmail.com>
2024-11-25 08:03:13 +0900

    rr

--- 자스링/11월17일/검색기능추가_게시물에 번호 출력하기 추가/www/src/main/java/www/silver/dao/IF_BoardDao.java
+++ 자스링/11월17일/검색기능추가_게시물에 번호 출력하기 추가/www/src/main/java/www/silver/dao/IF_BoardDao.java
@@ -9,6 +9,7 @@ public interface IF_BoardDao {
 	// 디비작업이 목적
 	// mybatis mapper랑 매핑해서 DB작업을 수행
 	public void insertBoard(BoardVO boardvo)throws Exception;
+	
 	public List<BoardVO> selectAll(Pagevo pagevo) throws Exception;
 	public void deleteBoard(String delno)throws Exception;
 	public BoardVO selectOne(String title)throws Exception;
